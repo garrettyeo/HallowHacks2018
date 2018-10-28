655e827e4e45ecaa61c8277bf1f9eeb16974c971
Ming Li <headhuntar@gmail.com>
2018-10-27 19:57:50 -0700

    added image files. added formating and reorganizing of content. need to finish organizing the homepage and styling, plus entering average-min-max data for all appliance types

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,32 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  title: string = 'homepage';
+  title: string = 'Is My Appliance Efficient?';
   provinces: Array<string> = PROVINCES;
   appliances: Array<string> = APPLIANCES;
   energyConsumption: number;
-  cost: number =0;
-  constructor() { }
+  currentAppliance: any = APPLIANCE_CONSUMPTION[3];
+  cost: number = 0;
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
 
-  showValue = (value: number) =>{
+  showValue = (value: number) => {
     this.energyConsumption = value;
     this.calcCost();
     console.log(value);
   }
 
-  calcCost = () =>{
-    let mul = 8.29;
-    this.cost = this.energyConsumption * (8.29/100);
+  calcCost = () => {
+    // let mul = 8.29;
+    this.cost = this.energyConsumption * (8.29 / 100);
   }
 
-  getWidth = () =>{
-    return this.energyConsumption/700*100;
+  getWidth = () => {
+    return (this.energyConsumption - this.currentAppliance.min) / this.currentAppliance.max * 100;
   }
 
 }
 
 const PROVINCES = ['BC', 'AB', 'SK', 'MB', 'ON', 'PQ', 'NS', 'NB', 'NL', 'PEI'];
 const APPLIANCES = ['Clothes Dryers', 'Clothes Washers', 'Dishwashers', 'Freezers', 'Electric Ranges (Cooktops and Ovens)', 'Refrigerators', 'Air conditioners'];
+const APPLIANCE_CONSUMPTION = [
+  {min: 1, max: 2},
+  {min: 1, max: 2},
+  {min: 1, max: 2},
+  {min: 100, max: 700},
+  {min: 1, max: 2},
+  {min: 1, max: 2},
+  {min: 1, max: 2}
+]
