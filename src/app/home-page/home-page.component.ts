e85007a3713ac34ef0256ff8ce196ea23a651b4f
Ming Li <headhuntar@gmail.com>
2018-10-28 11:40:48 -0700

    added ability to scan barcode

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Quagga from 'quagga';
 
 @Component({
   selector: 'app-home-page',
@@ -12,11 +13,100 @@ export class HomePageComponent implements OnInit {
   energyConsumption: number;
   currentAppliance: any = APPLIANCE_CONSUMPTION[3];
   cost: number = 0;
+  barcodeFound: number;
+  curYear: number = 2018;
+  applianceSelected: string = "Appliance";
+
   constructor() {
 
   }
 
   ngOnInit() {
+  }
+
+  startScanner = () => {
+    let scannerIsRunning: any = false;
+    Quagga.init({
+      inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        target: document.querySelector('#scanner-container'),
+        constraints: {
+          width: 480,
+          height: 320,
+          facingMode: "environment"
+        },
+      },
+      decoder: {
+        readers: [
+          "ean_reader"
+        ], debug: {
+          showCanvas: true,
+          showPatches: true,
+          showFoundPatches: true,
+          showSkeleton: true,
+          showLabels: true,
+          showPatchLabels: true,
+          showRemainingPatchLabels: true,
+          boxFromPatches: {
+            showTransformed: true,
+            showTransformedBox: true,
+            showBB: true
+          }
+        }
+      },
+
+    }, function (err) {
+      if (err) {
+        console.log(err);
+        return
+      }
+
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+
+      // Set flag to is running
+      scannerIsRunning = true;
+    });
+
+    Quagga.onProcessed(function (result) {
+      var drawingCtx = Quagga.canvas.ctx.overlay,
+        drawingCanvas = Quagga.canvas.dom.overlay;
+
+      if (result) {
+        if (result.boxes) {
+          drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+          result.boxes.filter(function (box) {
+            return box !== result.box;
+          }).forEach(function (box) {
+            Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+          });
+        }
+
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+        }
+      }
+    });
+
+    Quagga.onDetected(function (result) {
+      let barcode = document.getElementById("barcode-found");
+      if (result.codeResult.code.toString().length == 13) {
+        Quagga.enable = false;
+        Quagga.stop();
+        barcode.innerHTML = "Barcode Found: " + result.codeResult.code;
+      }
+    });
+
+  }
+
+  cameraOff = () => {
+    Quagga.stop();
+    document.getElementById("barcode-found").innerHTML = "Barcode Found:";
   }
 
   showValue = (value: number) => {
@@ -34,16 +124,20 @@ export class HomePageComponent implements OnInit {
     return (this.energyConsumption - this.currentAppliance.min) / this.currentAppliance.max * 100;
   }
 
+  selectAppliance = (appliance: string, index: number) => {
+    this.applianceSelected = appliance;
+    this.currentAppliance = APPLIANCE_CONSUMPTION[index];
+  }
+
 }
 
 const PROVINCES = ['BC', 'AB', 'SK', 'MB', 'ON', 'PQ', 'NS', 'NB', 'NL', 'PEI'];
-const APPLIANCES = ['Clothes Dryers', 'Clothes Washers', 'Dishwashers', 'Freezers', 'Electric Ranges (Cooktops and Ovens)', 'Refrigerators', 'Air conditioners'];
+const APPLIANCES = ['Clothes Dryers', 'Clothes Washers', 'Dishwashers', 'Freezers', 'Electric Ranges (Cooktops and Ovens)', 'Refrigerators'];
 const APPLIANCE_CONSUMPTION = [
-  {min: 1, max: 2},
-  {min: 1, max: 2},
-  {min: 1, max: 2},
-  {min: 100, max: 700},
-  {min: 1, max: 2},
-  {min: 1, max: 2},
-  {min: 1, max: 2}
+  { min: 721, max: 1280, average: 928 },
+  { min: 198, max: 354, average: 217 },
+  { min: 300, max: 484, average: 310 },
+  { min: 256, max: 621, average: 365 },
+  { min: 481, max: 683, average: 522 },
+  { min: 491, max: 703, average: 425 }
 ]
